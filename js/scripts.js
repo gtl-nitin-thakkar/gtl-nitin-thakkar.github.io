bd4fbbd14fb44c983862e751a8568fc4dfc665ea
jignesh-nagar <j.nagar@elsevier.com>
2019-11-15 13:32:08 +0530

    Add navigation drawer in main page

--- js/scripts.js
+++ js/scripts.js
@@ -111,11 +111,7 @@ function initDogma() {
         }
     }
     n();
-    function d() {
-        var a = document.querySelectorAll(".intense");
-        Intense(a);
-    }
-    d();
+
 // Owl carousel ------------------
     var heroslides = $(".hero-slider");
     heroslides.each(function(index) {
@@ -322,137 +318,7 @@ function initDogma() {
  if ($(window).width() < 1086){
  $("#wrapper").getNiceScroll().remove();
 }
-// Map  ------------------
-    $("#map-canvas").gmap3({
-        action: "init",
-        marker: {
-            values: [ {
-                latLng: [ 40.7143528, -74.0059731 ],
-                data: "Our office  - New York City",
-                options: {
-                    icon: "images/marker.png"
-                }
-            } ],
-            options: {
-                draggable: false
-            },
-            events: {
-                mouseover: function(a, b, c) {
-                    var d = $(this).gmap3("get"), e = $(this).gmap3({
-                        get: {
-                            name: "infowindow"
-                        }
-                    });
-                    if (e) {
-                        e.open(d, a);
-                        e.setContent(c.data);
-                    } else $(this).gmap3({
-                        infowindow: {
-                            anchor: a,
-                            options: {
-                                content: c.data
-                            }
-                        }
-                    });
-                },
-                mouseout: function() {
-                    var a = $(this).gmap3({
-                        get: {
-                            name: "infowindow"
-                        }
-                    });
-                    if (a) a.close();
-                }
-            }
-        },
-        map: {
-            options: {
-                zoom: 14,
-                zoomControl: true,
-                mapTypeControl: true,
-                scaleControl: true,
-                scrollwheel: false,
-                streetViewControl: true,
-                draggable: true,
-                styles: [ {
-                    featureType: "landscape",
-                    stylers: [ {
-                        saturation: -100
-                    }, {
-                        lightness: 65
-                    }, {
-                        visibility: "on"
-                    } ]
-                }, {
-                    featureType: "poi",
-                    stylers: [ {
-                        saturation: -100
-                    }, {
-                        lightness: 51
-                    }, {
-                        visibility: "simplified"
-                    } ]
-                }, {
-                    featureType: "road.highway",
-                    stylers: [ {
-                        saturation: -100
-                    }, {
-                        visibility: "simplified"
-                    } ]
-                }, {
-                    featureType: "road.arterial",
-                    stylers: [ {
-                        saturation: -100
-                    }, {
-                        lightness: 30
-                    }, {
-                        visibility: "on"
-                    } ]
-                }, {
-                    featureType: "road.local",
-                    stylers: [ {
-                        saturation: -100
-                    }, {
-                        lightness: 40
-                    }, {
-                        visibility: "on"
-                    } ]
-                }, {
-                    featureType: "transit",
-                    stylers: [ {
-                        saturation: -100
-                    }, {
-                        visibility: "simplified"
-                    } ]
-                }, {
-                    featureType: "administrative.province",
-                    stylers: [ {
-                        visibility: "off"
-                    } ]
-                }, {
-                    featureType: "water",
-                    elementType: "labels",
-                    stylers: [ {
-                        visibility: "on"
-                    }, {
-                        lightness: -25
-                    }, {
-                        saturation: -100
-                    } ]
-                }, {
-                    featureType: "water",
-                    elementType: "geometry",
-                    stylers: [ {
-                        hue: "#ffff00"
-                    }, {
-                        lightness: -25
-                    }, {
-                        saturation: -97
-                    } ]
-                } ]
-            }
-        }
-    });
+
 //  Contact form ------------------
     $("#contactform").submit(function() {
         var a = $(this).attr("action");
